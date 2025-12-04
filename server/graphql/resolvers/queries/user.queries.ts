618a24d90580c271be208c8534ae5848a441ede7
shayanashiq <shayanashiq8@gmil.com.com>
2025-12-04 22:55:12 +0500

    Add prompt

--- server/graphql/resolvers/queries/user.queries.ts
+++ server/graphql/resolvers/queries/user.queries.ts
@@ -1,5 +1,43 @@
 import { IGqlContext } from '@/types';
+import { log } from 'console';
 
 export const user = (_: unknown, args: unknown, { user }: IGqlContext) => {
   return user;
+};
+
+export const solveQuestion = (
+  _: unknown,
+  {
+    question,
+    options,
+    context,
+  }: {
+    question: string;
+    options: string[];
+    context: string;
+  }
+) => {
+  const optionsString = options
+    .map((option, index) => {
+      return index + ': ' + option;
+    })
+    .join('\n');
+  let prompt =
+    'You are a question solver. Solve following question and return the number of correct option\nExample of output {correctOption: 1}\n<question>\n';
+  prompt += question;
+  prompt +=
+    '\n</question>below are the options\n<options>\n' +
+    optionsString +
+    '\n</options>';
+
+  if (context) {
+    prompt +=
+      'Here is the context for this question\n<context>\n' +
+      context +
+      '\n</context>';
+  }
+  console.log(prompt);
+  return {
+    correctOption: 1,
+  };
 };
