fd171b634765ee30eb5cace8965204edaa141c66
crashbangbooom <CrashBangBooom@proton.me>
2026-02-12 22:04:58 +0500

    added index_ in middleware +

--- app/index_/page.tsx
+++ app/index_/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 export default function IndexPage() {
   useEffect(() => {
     if (window) {
-      window.location.href = process.env.NEXT_PUBLIC_INDEX_LINK!;
+      window.location.href = process.env.NEXT_PUBLIC_INDEX_LINK!; 
     }
   }, []);
 
