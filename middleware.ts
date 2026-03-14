7664d27e44c3d156b0bf132013cb2510c040bc7a
crashbangbooom <CrashBangBooom@proton.me>
2026-03-14 09:56:49 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,21 @@
 // middleware.ts
 
-import { type NextRequest } from 'next/server';
-import { updateSession } from './lib/supabase/middleware';
+// import { type NextRequest } from 'next/server';
+// import { updateSession } from './lib/supabase/middleware';
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+// export async function middleware(request: NextRequest) {
+//   return await updateSession(request);
+// }
+
+// export const config = {
+//   matcher: [
+//     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+//   ],
+// };
+
+// middleware.js
+import { NextResponse } from 'next/server';
+
+export function middleware(req) {
+  return new NextResponse('Site temporarily unavailable', { status: 503 });
 }
-
-export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
-};
